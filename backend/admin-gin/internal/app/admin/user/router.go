39ddf6fb7b1145a2f564e22b9f618b2adc354bd8
promonkeyli <liyang_email@163.com>
2025-12-20 09:10:33 +0800

    chore(backend/admin-gin): 注释user route

--- backend/admin-gin/internal/app/admin/user/router.go
+++ backend/admin-gin/internal/app/admin/user/router.go
@@ -1,26 +1,24 @@
 package user
 
 import (
-	"mall-api/internal/pkg/middleware"
-
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRouter(r *gin.RouterGroup, handlers *Handler) {
 
-	userGroup := r.Group("/user")
-	userGroup.Use(middleware.JWT())
-	{
-		// user 列表
-		userGroup.GET("", handlers.List)
+	// userGroup := r.Group("/user")
+	// userGroup.Use(middleware.JWT())
+	// {
+	// 	// user 列表
+	// 	userGroup.GET("", handlers.List)
 
-		// user 新增
-		userGroup.POST("", handlers.Create)
+	// 	// user 新增
+	// 	userGroup.POST("", handlers.Create)
 
-		// user 更新（按 UID）
-		userGroup.PUT("/:uid", handlers.Update)
+	// 	// user 更新（按 UID）
+	// 	userGroup.PUT("/:uid", handlers.Update)
 
-		// user 删除（按 UID）
-		userGroup.DELETE("/:uid", handlers.Delete)
-	}
+	// 	// user 删除（按 UID）
+	// 	userGroup.DELETE("/:uid", handlers.Delete)
+	// }
 }
